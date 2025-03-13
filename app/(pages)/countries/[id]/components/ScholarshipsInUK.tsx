99097f073fa62efe1f4700f057794a8514c9089d
Atiqa-Hameed <atiqahameed407@gmail.com>
2025-03-13 13:50:46 +0500

    scholarship in UK

--- app/(pages)/countries/[id]/components/ScholarshipsInUK.tsx
+++ app/(pages)/countries/[id]/components/ScholarshipsInUK.tsx
@@ -1,107 +1,75 @@
 import Image from "next/image";
 import React from "react";
-// import StudyInUk from "/StudyInUK.png";
-// import StudyInUk from "./StudyInUk";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
 interface ScholarshipsInUKProps {
   countryName: string;
-  scholarships: string[];
 }
+
 export const ScholarshipsInUK: React.FC<ScholarshipsInUKProps> = ({
   countryName,
-  // scholarships,
 }) => {
-  const healthcareItems = [
-    { text: "Public Health", image: "/countrypage/img3.svg" },
-    { text: "Healthcare Promotion", image: "/countrypage/img2.svg" },
-    { text: "Healthcare Information", image: "/countrypage/img5.png" },
-    { text: "Healthcare Management", image: "/countrypage/img4.svg" },
-    { text: "Healthcare Policy", image: "/countrypage/img1.svg" },
+  // Scholarships are defined inside the component, NOT as a prop
+  const scholarships = [
+    "Chevening Scholarships",
+    "Rhodes Scholarships",
+    "Great Scholarships",
+    "Gates Cambridge Scholarships",
+    "Rhodes Scholarships",
   ];
+
+  const scholarshipItems = [
+    { text: "Chevening Scholarships", image: "/countrypage/yellow.svg" },
+    { text: "Rhodes Scholarships", image: "/countrypage/orange.svg" },
+    { text: "Great Scholarships", image: "/countrypage/red.svg" },
+    { text: "Gates Cambridge Scholarships", image: "/countrypage/sky.svg" },
+    { text: "Rhodes Scholarships", image: "/countrypage/blue.svg" },
+  ];
+
   return (
     <>
-      <section>
-        <div
-          className="relative bg-cover mt-5 lg:mt-10 bg-center min-h-full flex flex-col items-center justify-center px-4 sm:px-6 lg:px-8 py-6"
-          style={{
-            backgroundImage: "url('/ScolarshipsInUK.png')",
-          }}
-        >
-          <div className=" absolute inset-0 bg-black opacity-60 z-0"></div>
-          <div className="flex flex-col lg:flex-row w-full justify-between items-start md:items-center">
-            <div className=" relative w-full md:w-[75%] lg:w-[50%] space-y-5 lg:p-3 text-white">
-              <h3 className="lg:mt-2 pt-4">Scholarships in {countryName}!</h3>
-              <p className="text-[#9D9D9D]">
-                Discover a range of scholarship opportunities on our dedicated
-                scholarship page. Whether you&#39;re a prospective student or
-                already enrolled, there are options available to help support
-                your education. Explore various scholarships based on merit,
-                need, or specific fields of study.
-              </p>
-            </div>
-        
-            <div className="hidden md:flex relative justify-center items-center w-full xl:w-[70%] text-white">
-              <div className="relative w-full pr-2 flex flex-col justify-center items-end h-[20vh]">
-                <div className="flex h-[50%] items-center justify-end">
-                  <p>Healthcare Promotion</p>
-                </div>
-                <div className="flex h-[50%] items-center pr-5 justify-end pt-4">
-                  <p>Healthcare Information</p>
-                </div>
-              </div>
-              {/* Center Section */}
-              <div className="w-full flex flex-col justify-start">
-                <div className="flex justify-center text-center m-2">
-                  <p>Public Health</p>
-                </div>
-                <div className="relative ">
-                  <Image
-                    src="/countrypage/studyinuk.svg"
-                    alt="BSC Physiology"
-                    width={400}
-                    height={400}
-                  />
-                  <div className="absolute inset-0 top-3 xl:top-4 flex justify-center items-center w-full h-full">
-                    <div className="flex justify-center items-center  text-center w-[15%]">
-                    <p className="text-center  text-white md:text-black font-semibold ">
-                      {" "}
-                      Scholarships
-                    </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Right Section with Reduced Border Height */}
-              <div className="relative w-full pl-2 flex flex-col justify-center items-start h-[20vh]">
-                <div className="flex h-[50%] items-center ">
-                  <p>Healthcare Management</p>
-                </div>
-                <p className="flex h-[50%] items-center  pl-5 pt-4">
-                  Healthcare Policy
-                </p>
-              </div>
-            </div>
-            <div className="relative flex flex-col md:hidden space-y-4 my-4">
-              <h5 className="text-white"> Scholarships in {countryName}</h5>
-              {healthcareItems.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex items-center space-x-4 text-white "
-                >
-                  <Image
-                    src={item.image}
-                    alt={item.text}
-                    width={30}
-                    height={30}
-                  />
-                  <p>{item.text}</p>
-                </div>
-              ))}
-            </div>
-          </div>
+      {/* Scholarship Timeline Section */}
+      <section className="hidden md:flex flex-col items-center bg-black text-white py-8">
+        <h4>
+          Scholarships in {countryName}!
+        </h4>
+
+        {/* Timeline Image */}
+        <div className="relative w-full flex justify-center mt-4">
+          <Image
+            src="/countrypage/scholarship.svg"
+            alt="Scholarships Timeline"
+            width={900}
+            height={300}
+            className="w-full md:w-[80%]  lg:w-[70%]"
+          />
+        </div>
+
+        {/* Scholarship List Below Image */}
+        <div className="flex w-full md:w-[95%] lg:w-[85%] mt-4">
+          {scholarships.map((scholarship, index) => (
+            <p key={index} className="text-xs sm:text-sm lg:text-base text-center w-2/5">
+              {scholarship}
+            </p>
+          ))}
         </div>
       </section>
+      <div className="md:hidden flex flex-col items-start bg-black text-white py-8 px-6">
+        <h4>
+          Scholarships in {countryName}!
+        </h4>
+
+        <div className="flex flex-col space-y-3 mt-4">
+          {scholarshipItems.map((item, index) => (
+            <div key={index} className="flex items-center space-x-">
+              <Image src={item.image} alt={item.text} width={15} height={15} />
+              <p>{item.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Explore Scholarships Section */}
       <section
         className="relative mt-10 text-white bg-[#FCE7D2]"
         style={{
---
+++ app/adminportal/(auth)/login/page.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Image from "next/image";
+import { useState } from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address."),
+  password: z.string().min(6, "Password must be at least 6 characters."),
+  rememberMe: z.boolean().optional(),
+});
+
+const Page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast.success("Form submitted successfully!", {
+      description: "We'll get back to you soon.",
+    });
+    console.log(values);
+  }
+
+  return (
+    <>
+      <div
+        className="relative w-[98%] my-4 mx-auto bg-cover bg-center bg-no-repeat rounded-3xl h-[96vh]  md:h-[96vh] flex justify-center items-center"
+
+style={{ backgroundImage: "url('/adminportal/loginbackgroundimg.svg')" }}
+      >
+        <div className="flex flex-col justify-center items-center text-center w-full p-4">
+          <Image
+            src="/adminportal/wwah.svg"
+            alt="WWAH Logo"
+            width={130}
+            height={130}
+            className=" mb-4"
+          />
+
+          <h4>Welcome Back!</h4>
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full md:w-[55%] lg:w-[40%] xl:w-[30%]">
+              {/* Email Field */}
+              <div className="flex flex-col text-start">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email Address</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type="text"
+                            placeholder="Enter your email address"
+                            className="placeholder-[#313131] placeholder:text-sm xl:placeholder:text-base pl-10 w-full rounded-lg py-5 md:py-6 border border-gray-300"
+                            {...field}
+
+                          />
+                          <span className="absolute left-3 top-1/2 -translate-y-1/2">
+                            <Image
+                              src="/DashboardPage/letter.svg"
+                              alt="User Icon"
+                              width={20}
+                              height={20}
+                              className="w-5 h-5 text-black"
+                            />
+                          </span>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* Password Field with Eye Icon */}
+              <div className="flex flex-col text-start">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative w-full">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Enter password"
+                            className="placeholder-[#313131] placeholder:text-sm xl:placeholder:text-base pl-10 w-full rounded-lg py-5 md:py-6 border border-gray-300"
+
+                            {...field}
+                          />
+                          <span className="absolute left-4 top-1/2 -translate-y-1/2">
+                            <Image
+                              src="/adminportal/key.svg"
+                              alt="Password Icon"
+                              width={20}
+                              height={20}
+                              className="w-4 h-4 text-black"
+                            />
+                          </span>
+                          {/* Eye Icon for Toggle Password */}
+                          <span
+                            className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                            onClick={() => setShowPassword(!showPassword)}
+                          >
+                            {showPassword ? (
+                              <EyeOff className="w-5 h-5 text-gray-600" />
+                            ) : (
+                              <Eye className="w-5 h-5 text-gray-600" />
+                            )}
+                          </span>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              {/* Remember Me & Forgot Password */}
+              <div className="flex justify-between items-center w-full">
+                <div className="flex items-center space-x-2">
+                  <FormField
+                    control={form.control}
+                    name="rememberMe"
+                    render={({ field }) => (
+                      <FormItem className="flex items-center">
+                        <FormControl>
+                          <Checkbox
+                            checked={field.value}
+                            onCheckedChange={field.onChange}
+                          />
+                        </FormControl>
+                        <FormLabel className="ml-2">Remember me</FormLabel>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Link href="/adminportal/forgetpassword" className="text-[#C06A17] text-sm hover:underline">
+                  Forgot Password?
+                </Link>
+
+              </div>
+
+              {/* Sign In Button */}
+              <Button
+                type="submit"
+                className="w-full bg-red-600 hover:bg-red-500 text-white py-5 md:py-6 rounded-lg text-base"
+              >
+                Sign In
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
