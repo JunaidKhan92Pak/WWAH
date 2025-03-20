6d138bd218db7e0608202a5ea38a6de0ed7adc66
Junaid Khan <junaidsultan92pk@gmail.com>
2025-03-20 11:38:10 +0500

    Resolve the merge changes

--- app/(pages)/courses/[id]/components/ProgressSection.tsx
+++ app/(pages)/courses/[id]/components/ProgressSection.tsx
@@ -9,6 +9,7 @@ import { ReactNode } from 'react';
 import { useUserStore } from '@/store/userStore';
 import { calculateGradeSuccess } from '@/utils/calculateGradeSuccess';
 import { calculateTuitionFeeSuccess } from '@/utils/calculateTuitionFeeSuccess';
+import Link from 'next/link';
 
 
 
@@ -121,7 +122,6 @@ export const ProgressSection = ({ data }: { data: progressProps['data'] }) => {
     const englishSuccess = calculateEnglishSuccess(userScore, requiredScore);
 
     useEffect(() => {
-
         // Dummy values for demonstration:
         const studentDegree = user?.majorSubject.highestQualification || ""
             ; // Example: student's degree
@@ -131,7 +131,7 @@ export const ProgressSection = ({ data }: { data: progressProps['data'] }) => {
         // Assume user's grade and grading scale come from user.profile
         // For example, user's previous grading scale is "percentage" and their score is 45.
         const requiredGradeValue = extractGrades(data.entry_requirement).percentage; // e.g., 60%
-        const studentGrade = user?.majorSubject.previousGradingScore || 45; // Replace with actual value if available
+        const studentGrade = user?.majorSubject.previousGradingScore || 0; // Replace with actual value if available
         const studentScale: "percentage" | "letter" | "cgpa" | "passfail" = user?.majorSubject?.previousGradingScale || "percentage";
         const gradeSuccess = calculateGradeSuccess(
             studentGrade,
@@ -264,6 +264,15 @@ export const ProgressSection = ({ data }: { data: progressProps['data'] }) => {
             <p className="text-gray-600 mb-2">
                 Your application success chances are:
             </p>
+            {user ? <></> : (
+                <div className="text-center py-2 lg:px-4">
+                    <div className="p-2 bg-blue-400 items-center text-white leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                        <span className="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
+                        <span className="font-semibold mr-2 text-left flex-auto"> <Link href={"/login"} className='underline' >Complete</Link>  your profile to get your success chance </span>
+                        <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
+                    </div>
+                </div>
+            )}
 
             <div className="flex flex-col md:flex-row justify-center gap-8 w-full lg:w-[85%]">
                 {/* Left side Academic Results */}
